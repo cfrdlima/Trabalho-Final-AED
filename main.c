8147d7146f3ff7271b53cb4aee7405737ddb21bb
cfrdlima <cfrdlxava@gmail.com>
2023-08-17 23:25:58 -0300

    Estrutura Inicial do Trabalho

--- main.c
+++ main.c
@@ -1,7 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "matrix.h"
+
+Matrix *matrix_create()
+{
+  // Criacao da Matriz
+}
+
+void matrix_destroy(Matrix *m)
+{
+  // Destruicao da Matriz
+}
+
+void matrix_print(Matrix *m)
+{
+  // Print da Matriz
+}
+
+Matrix *matrix_add(Matrix *m, Matrix *n)
+{
+  // Soma das Matrizes
+}
+
+Matrix *matrix_multiply(Matrix *m, Matrix *n)
+{
+  // Multiplicacao das Matrizes
+}
+
+Matrix *matrix_transpose(Matrix *m)
+{
+  // Transpose da Matriz
+}
+
+float matrix_getelem(Matrix *m, int x, int y)
+{
+  // Pega Elemento da Matriz
+}
+
+void matrix_setelem(Matrix *m, int x, int y, float elem)
+{
+  // Set Elemento da Matriz
+}
 
 int main()
 {
-  printf("Hello world!");
-  printf("alteracao 2");
+  /* Inicializacao da aplicacao ... */
+  Matrix *A = matrix_create();
+  matrix_print(A);
+
+  Matrix *B = matrix_create();
+  matrix_print(B);
+
+  Matrix *C = matrix_add(A, B);
+  matrix_print(C);
+  matrix_destroy(C);
+
+  C = matrix_multiply(A, B);
+  matrix_print(C);
+  matrix_destroy(C);
+
+  C = matrix_transpose(A);
+  matrix_print(C);
+  matrix_destroy(C);
+
+  matrix_destroy(A);
+  matrix_destroy(B);
+  return 0;
 }
---
+++ matrix.h
@@ -0,0 +1,27 @@
+struct matrix
+{
+  struct matrix *right;
+  struct matrix *below;
+  int line;
+  int column;
+  float info;
+};
+
+typedef struct matrix Matrix;
+
+// create Matrix
+Matrix *matrix_create(void);
+// destroy Matrix
+void matrix_destroy(Matrix *m);
+// print Matrix
+void matrix_print(Matrix *m);
+// add Matrix
+Matrix *matrix_add(Matrix *m, Matrix *n);
+// multiply Matrix
+Matrix *matrix_multiply(Matrix *m, Matrix *n);
+// transpose Matrix
+Matrix *matrix_transpose(Matrix *m);
+// getElem Matrix
+float matrix_getelem(Matrix *m, int x, int y);
+// setElem Matrix
+void matrix_setelem(Matrix *m, int x, int y, float elem);
