60ced8b573b9b6d2c7d3d355244b8f9ba03958e4
claudinei <claudinei@mertins.com.br>
2023-08-20 20:02:15 -0300

    Implementacao das funcoes principais.

--- matrix.h
+++ matrix.h
@@ -9,6 +9,10 @@ struct matrix
 
 typedef struct matrix Matrix;
 
+// create node
+Matrix *createNode(int row, int col, float value);
+// add node
+void addNode(Matrix **head, int row, int col, float value);
 // create Matrix
 Matrix *matrix_create(void);
 // destroy Matrix
